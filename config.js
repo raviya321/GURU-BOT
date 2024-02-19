2364e08220d64a187ccb15d6d82b5cda3c8da85c
raviya321 <159415931+raviya321@users.noreply.github.com>
2024-02-19 12:32:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+global.owne= [
+  ['94757660788', 'GURU', true],
+  ['94757660788', 'Ash', true], 
   [''] 
 ] //Number of owners
 
